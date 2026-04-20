f82762285753ad75cf00c3f85f99e52737baca21
Leonardo Mazzon <leonardomazzon5@gmail.com>
2026-04-20 21:11:31 +0200

    Added test on bst

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from tests.test_bst import test_insert
+#!/usr/bin/env python3
+
+from tests.test_bst import test_insert, test_insert_and_remove
 
 
 def main():
     test_insert()
+    test_insert_and_remove()
 
 
 if __name__ == "__main__":
--- tests/test_bst.py
+++ tests/test_bst.py
@@ -1,6 +1,12 @@
 from random import Random
+from sys import stderr
 
-from src.BST.BST import BinarySearchTree
+if __name__ == "__main__":
+    stderr.write("[ERROR] This file is a module\n")
+    exit(-1)
+
+
+from src.BST.BST import BST
 
 
 def test_insert():
@@ -8,7 +14,7 @@ def test_insert():
     # the random interval to generate numbers to be inserted
     rand_range = (0, 10000)
     to_insert = 1000
-    bst = BinarySearchTree()
+    bst = BST()
     arr = []
 
     for _ in range(to_insert):
@@ -16,7 +22,7 @@ def test_insert():
         # we don't want to insert the same items into the array or the bst
         if n not in arr:
             arr.append(n)
-            bst.insert(n)
+            bst.insert_key(n)
 
     arr.sort()
 
@@ -24,9 +30,26 @@ def test_insert():
     assert arr == bst.inorder()
 
 
-def main():
-    test_insert()
+def test_insert_and_remove():
+    rng = Random()
+    # the random interval to generate numbers to be inserted
+    rand_range = (0, 10000)
+    to_insert = 1000
+    bst = BST()
+    arr = []
+    for _ in range(to_insert):
+        if len(arr) == 0 or rng.randint(0, 1) == 0:
+            n = rng.randint(rand_range[0], rand_range[1])
+            # we don't want to insert the same items into the array or the bst
+            if n not in arr:
+                arr.append(n)
+                bst.insert_key(n)
+        else:
+            n = rng.randint(0, len(arr) - 1)
+            bst.remove(bst.find(arr[n]))
+            arr.remove(arr[n])
 
+    arr.sort()
 
-if __name__ == "__main__":
-    main()
+    # the tree is a bst if and only if its inorder visit is sorted
+    assert arr == bst.inorder()
