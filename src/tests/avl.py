1323fecdc0fbd1532e45135d6aa265e6fd0d9f2b
Damiano Netto <damiano.netto.05@gmail.com>
2026-06-18 17:26:29 +0200

    Implemented insert/remove methods in AVL

--- src/tests/avl.py
+++ src/tests/avl.py
@@ -1,16 +1,20 @@
 from random import Random
 from sys import stderr
 
+from trees.avl import AVL
+
 if __name__ == "__main__":
     stderr.write("[ERROR] This file is a module\n")
     exit(-1)
 
 
-from trees.avl import AVL
-
-
 def test_avl_insert():
-    print("test_avl_1")
+    x = AVL()
+    x.insert_key(5)
+    x.insert_key(6)
+    print(x)
+    x.remove_key(5)
+    print(x)
 
 def test_avl_insert_and_remove():
     print("test_avl_2")
--- src/trees/avl.py
+++ src/trees/avl.py
@@ -9,38 +9,46 @@ if __name__ == "__main__":
     
 class AVLNode(Node):
     height: int | None
+    parent: "AVLNode | None"
 
-    def __init__(
-        self, key: int, left: "AVLNode | None" = None, right: "AVLNode | None" = None
-    ):
+    def __init__(self, key: int, left: "AVLNode | None" = None, right: "AVLNode | None" = None):
         super().__init__(key, left, right)
         self.height = None
 
-
-def height(node):
+def height(node : Node | None):
     if node is None:
         return 0
     if getattr(node, "height", None) is None:
         node.height = 1 + max(height(node.left), height(node.right))
     return node.height
 
-
-def invalidate_height(node):
+#Rende nulle tutte le altezze
+def invalidate_height(node : AVLNode | None):
     while node is not None:
         node.height = None
         node = node.parent
 
 
 class AVL(BST):
+    root: AVLNode | None
+
     def __init__(self, root: AVLNode | None = None):
         super().__init__(root)
+        self.root = root
+
+    #fattore bilancio di un singolo nodo
+    def balance_factor(self, node: Node | None):
+        if node is None:
+            return 0
+        #height ritorna 0 se il nodo è nullo!
+        return height(node.left) - height(node.right)
 
     def rotate_right(self, target: Node | None):
         if target is None:
             return
         y = target.left
 
-        self.rotate_right(target)
+        BST.rotate_right(self, target)
 
         invalidate_height(target)
         invalidate_height(y)
@@ -50,13 +58,51 @@ class AVL(BST):
             return
         y = target.right
 
-        self.rotate_left(target)
+        BST.rotate_left(self, target)
 
         invalidate_height(target)
         invalidate_height(y)
 
-    def insert(self, n: Node):
-        pass
+    def insert_key(self, key: int):
+        return self._insert(AVLNode(key))
+    
+    def _insert(self, n: AVLNode):
+        BST.insert(self, n)
 
-    def remove(self, target: Node | None):
-        pass
+        self.balance_avl(n)
+
+    def remove_key(self, key: int):
+        target = self.find(key)
+        if target is None: #Previene cancellazione chiave che non esiste (non trovata)
+            return
+        else:
+            self._remove(target)
+
+    def _remove(self, target: AVLNode):
+        start = target.parent #attenzione se target era radice (?)
+
+        if target.left is not None and target.right is not None:
+            succ_node = self.nxt(target) #Mi salvo il successore che troverà BST da sostituire al nodo
+            start = succ_node.parent  #qui è da dove parte ribilanciamento
+
+        BST.remove(self, target)
+
+        self.balance_avl(start)
+
+    def balance_avl(self, x ):
+        while x is not None:
+            invalidate_height(x)
+            
+            balance_factor = self.balance_factor(x)
+            
+            if balance_factor > 1:
+                if self.balance_factor(x.left) < 0:
+                    self.rotate_left(x.left)
+                self.rotate_right(x)
+            elif balance_factor < -1:
+                if self.balance_factor(x.right) > 0:
+                    self.rotate_right(x.right)
+                self.rotate_left(x)
+                
+            #Mi sposto in su, fin tanto che x è la radice, poi mi fermerò (vedi while)
+            x = x.parent
