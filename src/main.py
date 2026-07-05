d03cc45de8d7239c91cd1b2770c88040a7321992
Damiano Netto <damiano.netto.05@gmail.com>
2026-07-05 09:09:22 +0200

    Implemented first version of test cases and misurations for AVL

--- src/main.py
+++ src/main.py
@@ -7,19 +7,19 @@ from tests.rbt import test_rbt_insert_height, test_rbt_insert_and_remove
 def main():
     print("AVL TESTS:\n")
 
-    test_avl_insert()
+    #test_avl_insert()
     test_avl_insert_and_remove()
 
-    print("\nBST tests:\n")
+    # print("\nBST tests:\n")
 
-    test_bst_insert()
-    test_bst_insert_and_remove()
-    test_bst_rotate()
+    # test_bst_insert()
+    # test_bst_insert_and_remove()
+    # test_bst_rotate()
 
-    print("\nRBT tests:\n")
+    # print("\nRBT tests:\n")
 
-    test_rbt_insert_height()
-    test_rbt_insert_and_remove()
+    # test_rbt_insert_height()
+    # test_rbt_insert_and_remove()
 
 if __name__ == "__main__":
     main()
--- src/tests/avl.py
+++ src/tests/avl.py
@@ -1,5 +1,8 @@
 from random import Random
 from sys import stderr
+import time
+from statistics import median
+import matplotlib.pyplot as plt
 
 from trees.avl import AVL
 
@@ -14,21 +17,57 @@ def test_avl_insert():
     rand_range = (0, 50)
     to_insert = 10
     
+    start = time.perf_counter()
+
     for _ in range(to_insert):
         x.insert_key(r.randint(rand_range[0], rand_range[1]))
     print(x)
 
+    stop = time.perf_counter()
+
+    duration = stop - start
+
+    print(duration)
+
 def test_avl_insert_and_remove():
     r = Random()
-    x = AVL()
-    rand_range = (0, 50)
-    to_insert = 10
-    to_remove = 5
+    duration = list()
     
-    for _ in range(to_insert):
-        x.insert_key(r.randint(rand_range[0], rand_range[1]))
-    print(x)
+    for n in range(0,50000,500):
+        print(f"Ora faccio il caso n={n}")
+        x = AVL()
+
+        #Creo lista
+        valori = list(range(0,n+1))
+        r.shuffle(valori)
+
+        #Metto n valori casuali nell'albero
+        for i in range(0,n):
+            x.insert_key(valori[i])
+
+        #Inizialmente inserirò ultimo indice (unico che manca ancora)
+        ind = n
+        tempi = list()
+
+        #Faccio mediana su un campione di 300 tempi, per ogni n
+        for _ in range(300):
+            #MISURAZIONE
+            start = time.perf_counter()
+            x.insert_key(valori[ind])
+            stop = time.perf_counter()
+
+            #Aggiungo uno dei 300 tempi: poi farò la mediana di tutti quelli raccolti
+            tempi.append(stop - start)
+
+            ind = r.randint(0,n)
+            x.remove_key(valori[ind])
+        duration.append(median(tempi))
     
-    for _ in range(to_remove):
-        x.remove_key(r.randint(rand_range[0], rand_range[1]))
-    print(x)
+    plt.figure(figsize=(8,5))
+    plt.plot(list(range(0,50000,500)), duration, marker='.')
+    plt.xlabel("Nodi (n)")
+    plt.ylabel("Tempo (s)")
+    plt.title("AVL tempo inserimento")
+    plt.grid(True)
+
+    plt.show()
