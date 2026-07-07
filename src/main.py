55b58b237f1fa8cfb09b1b5af14a92e8df078b42
Sebastiano Babich <babich.sebastiano@proton.me>
2026-07-07 08:44:26 +0200

    Plotting as flags
    added flags to avoid continuous editing of main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,22 @@ from tests.test_insertions import insertion_plot
 from tests.test_rotations import rotations_plot
 from tests.test_heights import heights_plot
 
+import argparse
 
 def main():
-    #insertion_plot(TreeType.AVL, TreeType.BST, TreeType.RBT)
-    #rotations_plot(TreeType.AVL, TreeType.RBT)
-    heights_plot(TreeType.AVL, TreeType.BST, TreeType.RBT)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--plot", nargs="+", choices=["insertion", "rotations", "heights"], default=["insertion", "rotations", "heights"], help="What graph(s) to plot")
+    parser.add_argument("--trees", nargs="+", choices=["AVL", "BST", "RBT"], default=["AVL", "BST", "RBT"], help="What tree(s) to plot")
+    args = parser.parse_args()
 
+    trees = [TreeType(t) for t in args.trees]
+
+    if "insertion" in args.plot:
+        insertion_plot(*trees)
+    if "rotations" in args.plot:
+        rotations_plot(*trees)
+    if "heights" in args.plot:
+        heights_plot(*trees)
 
 if __name__ == "__main__":
     main()
